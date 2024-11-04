6d7e17a0d691f311299422a4981d689c191f1882
Dantrottel <daniel.aguayo2001@alumnos.ubiobio.cl>
2024-11-04 12:05:31 -0300

    login incompleto

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 
 const express = require('express');
-const routerApi = require('./src/routes'); // Asegúrate de la ruta según tu estructura de carpetas
+const router = require('./src/routes'); // Asegúrate de la ruta según tu estructura de carpetas
 const app = express();
 const PORT = process.env.PORTmiddleware;
 
 app.use(express.json()); // Para que pueda recibir JSON en las solicitudes
 
 // Configuración de rutas
-routerApi(app);
+router(app);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
