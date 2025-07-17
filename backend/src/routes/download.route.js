6e61f20fdd36a5edf6323e64870f517b432cb575
Dantrottel <daniel.aguayo2001@alumnos.ubiobio.cl>
2025-07-17 16:51:26 -0400

    fix

--- backend/src/routes/download.route.js
+++ backend/src/routes/download.route.js
@@ -1,16 +1,22 @@
 import path from 'path';
+import fs from 'fs';
 import express from 'express';
-
+export {verifySession} from '../middlewares/verifySession.js'
 const router = express.Router();
 
 router.get('/descargar/:filename', (req, res) => {
   const filename = req.params.filename;
   const filePath = path.resolve('uploads/propuestas', filename);
 
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).send('Archivo no encontrado');
+  }
+
+  res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
   res.download(filePath, filename, (err) => {
     if (err) {
       console.error('Error al descargar archivo:', err);
-      res.status(404).send('Archivo no encontrado');
+      res.status(500).send('Error al descargar el archivo');
     }
   });
 });
